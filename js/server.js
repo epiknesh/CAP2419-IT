3fc943d23e840a67810ad13f6fb7456849f76c68
imchen26 <ingridchen1026@gmail.com>
2025-03-07 01:58:48 +0800

    update websocket

--- js/server.js
+++ js/server.js
@@ -245,7 +245,35 @@ app.get('/fleetPersonnel', async (req, res) => {
 });
 
 
+// WEB SOCKET (WALKIE-TALKIE)
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ port: 8080 });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+    clients.push(ws);
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        
+        // Broadcast message to all clients
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server running on ws://localhost:8080');
 
 
 
