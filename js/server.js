51895c91d9696776158b68b5b22ed824033b73cf
epiknesh <seanbartolome7slm@gmail.com>
2025-06-05 16:22:13 +0800

    Update server.js

--- js/server.js
+++ js/server.js
@@ -20,6 +20,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// GPS routes under /api
+app.use("/api", gpsServer);
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
